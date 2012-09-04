40aadf019f31b3deb3fbb287f97d50735a0cc0c5
Daniel Lamando <me@danopia.net>
2012-09-04 16:53:23 +0200

    A wild app market appears

---
+++ app.rb
@@ -0,0 +1,58 @@
+require 'sinatra'
+require 'haml'
+require 'sequel'
+require 'logger'
+require 'json'
+
+if ENV.key? 'DATABASE_URL'
+  DB = Sequel.connect ENV['DATABASE_URL']
+  raise 'Not migrated' unless DB.table_exists?(:repos)
+elsif ENV['RACK_ENV'] != 'production'
+  DB = Sequel.sqlite
+  require './migrate'
+else
+  raise 'Running production mode without a database'
+end
+DB.loggers << Logger.new(STDOUT) if ENV['RACK_ENV'] != 'production'
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
+
+set :protection, :except => :frame_options
+
+get '/all' do
+  @repos = {}
+  DB[:repos].each do |repo|
+    @repos[repo[:id]] = JSON.parse repo[:json]
+  end
+  
+  DB[:apps].each do |app|
+    manifest = JSON.parse app[:json]
+    next unless repo = @repos[app[:repo_id]]
+    next unless entry = repo.find {|e| e['name'] == manifest['name'] }
+    entry['installed'] = manifest
+  end
+  
+  @apps = @repos.values.map {|r| r['apps'].map {|a| a.merge('repo' => r) } }.flatten
+  haml :all, :format => :html5
+end
+
+get '/:repo_id?' do |repo_id|
+  repo_id = repo_id ? repo_id.to_i : 1
+  
+  @repos = DB[:repos].map {|r| r.merge(:data => JSON.parse(r[:json])) }
+  @repo = @repos.find {|r| r[:id] == repo_id }
+
+  @apps = @repo[:data]['apps']
+  
+  DB[:apps].where(:repo_id => repo_id).each do |app|
+    manifest = JSON.parse app[:json]
+    next unless entry = @repo.find {|e| e['name'] == manifest['name'] }
+    entry['installed'] = manifest
+  end
+  
+  haml :repo, :format => :html5
+end
+
---
+++ migrate.rb
@@ -0,0 +1,45 @@
+unless Object.const_defined? 'DB'
+  raise 'Not given a database to migrate' unless ENV.key? 'DATABASE_URL'
+  
+  require 'sequel'
+  require 'logger'
+
+  DB = Sequel.connect ENV['DATABASE_URL']
+  DB.loggers << Logger.new(STDOUT) if ENV['RACK_ENV'] != 'production'
+end
+
+unless DB.table_exists? :repos
+  DB.create_table :repos do
+    primary_key :id
+
+    Fixnum :user_id, :null => false
+    String :json, :null => false, :text => true
+
+    DateTime :created_at, :null => false
+    index :created_at
+    DateTime :updated_at
+  end
+  
+  DB.create_table :apps do
+    primary_key :id
+    foreign_key :repo_id, :repos, :null => false
+    index       :repo_id
+
+    Fixnum :user_id, :null => false
+    String :json, :null => false, :text => true
+    String :name, :null => false
+
+    DateTime :created_at, :null => false
+    index :created_at
+    DateTime :updated_at
+  end
+
+  # Some test data
+  if ENV['RACK_ENV'] != 'production'
+    require 'open-uri'
+    require 'json'
+    data = open('http://protonet-apps.github.com/apps.json').read
+    DB[:repos] << {:user_id => 0, :json => data, :created_at => Time.now}
+  end
+end
+
